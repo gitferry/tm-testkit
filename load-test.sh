7f1cf59bc38736ac28cda7597f8140ad1620f193
gitferry <greferry@gmail.com>
2021-07-06 11:02:55 +0800

    4 nodes test

--- load-test.sh
+++ load-test.sh
@@ -1,4 +1,4 @@
 tm-load-test -c 1 -T 10 -r 100 -s 250 \
-	    --broadcast-tx-method commit \
+	    --broadcast-tx-method async \
 	    --endpoints ws://121.199.17.121:26657/websocket \
 	    --stats-output ./stats.csv
